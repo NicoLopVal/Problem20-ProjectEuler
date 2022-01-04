689eecd8f8532e0aee92e09b9e3158db7e85b953
nikil <nikil@DESKTOP-73MHGNO>
2022-01-03 21:59:15 -0500

    Add project files.

---
+++ Problem20-ProjectEuler/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Numerics;
+
+BigInteger value = 1;
+
+for(int i = 100; i > 0; i--)
+{
+    value = value * i;
+}
+
+char[] resultDigits = value.ToString().ToCharArray();
+double sum = 0;
+
+foreach(char c in resultDigits)
+{
+    sum = sum + Convert.ToInt32(c.ToString());
+}
+
+Console.WriteLine("The sum of the digits in the number 100! is: " + sum);
+
